eeca1abfc2e7d62e90b892ed4b647977f6009a74
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-01-24 22:28:45 +0100

    REFRACTOR .js code to improve efficiency

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -4,19 +4,18 @@ const convertToCelsius = function(far) {
   // answer of 0.0 instead of just 0. It avoids needless 1 decimal place answers
   // in such cases. 
 
-  let conversion = Math.round(((far - 32)*5/9) * 10) / 10;
+  return Math.round(((far - 32)*5/9) * 10) / 10;
   /* let answer = conversion.toFixed(1);
   return answer; */
-  return conversion;
 
 };
 
 const convertToFahrenheit = function(cel) {
 
-  let conversion = Math.round( ((cel * 1.8) + 32) * 10) / 10;
+  return Math.round( ((cel * 1.8) + 32) * 10) / 10;
   /* let answer = conversion.toFixed(1);
   return answer; */
-  return conversion;
+  
 };
 
 // Do not edit below this line
