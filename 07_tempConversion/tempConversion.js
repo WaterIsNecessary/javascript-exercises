192661ba2c7e9cd17ac141b0b52dada7a80f3e36
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-01-24 22:24:31 +0100

    UPDATE .js code such that first test passes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+
+  // Using Math.round as opposed to toFixed solves the issue of returning the
+  // answer of 0.0 instead of just 0. It avoids needless 1 decimal place answers
+  // in such cases. 
+
+  let conversion = Math.round(((far - 32)*5/9) * 10) / 10;
+  /* let answer = conversion.toFixed(1);
+  return answer; */
+  return conversion;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+
+  let conversion = Math.round( ((cel * 1.8) + 32) * 10) / 10;
+  /* let answer = conversion.toFixed(1);
+  return answer; */
+  return conversion;
 };
 
 // Do not edit below this line
