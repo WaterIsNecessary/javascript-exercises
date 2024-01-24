771a899a56efdacb49b5bb99b4b310a30d619a9b
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-01-24 20:43:29 +0100

    COMMENT code in .js to improve understanding

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -28,6 +28,18 @@
 // Alternate more efficient method
 
 const removeFromArray = function(arr, ...theArgs) {
+
+    //  Filter creates a new array with array elements that pass a test.
+    //  The filter method uses a callback function we have designed here as an
+    //  arrow function.
+
+    // Includes allows us to check if an element is present in an array.
+    // We check individually if the elements of arr are NOT included in theArgs.
+    // If this returns true on each check, this element is passed into the new
+    // array created by the filter method.
+    // N.B. Includes is just a check, it does not create a new array from its
+    // true results, thus the inclusion of the filter method to do just that.
+
     return arr.filter((valueF) => !theArgs.includes(valueF));
 }
 
