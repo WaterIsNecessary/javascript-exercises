261b56910ee3077d14d363d99b03e705c4bc7a39
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-01-24 21:30:29 +0100

    UPDATE .js code such that first test passes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let sum = null;
+
+    if (num1 < num2 && num1 >= 0 && num2 >=0 && Number.isInteger(num1) && Number.isInteger(num2)) {
+
+        for (i = num2; i >= num1; i--) {
+            sum += i;
+        }
+
+    } else if (num1 > num2 && num1 >= 0 && num2 >=0 && Number.isInteger(num1) && Number.isInteger(num2)) {
+
+        for (i = num1; i >= num2; i-- ) {
+            sum += i;
+        }
+
+    } else {
+        sum = "ERROR"
+    }
+
+    return sum;
 
 };
 
