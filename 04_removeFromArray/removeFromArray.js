06c4a4058edf4de03e104cb196ab0c920134cac3
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-01-24 15:53:50 +0100

    UPDATE .js such that the first test passes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
 
+    let outputArr = arr;
+
+    // Iterate through all values to be removed
+
+    for (i = 0; i < theArgs.length; i++) {
+
+        let removeThis = theArgs[i];
+
+        // Compare all values to be removed individually, iterated through all
+        // values in the provided array
+
+        for (j = 0; j < arr.length; j++) {
+            
+            if (removeThis === arr[j]) {
+                
+                outputArr.splice(j,1);
+
+            }
+        }
+    }
+
+    return outputArr;
 };
 
 // Do not edit below this line
