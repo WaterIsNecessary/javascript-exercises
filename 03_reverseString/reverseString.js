0b14b3cfa4262748266df3e19b13f0c8e0571422
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-01-24 14:04:39 +0100

    UPDATE .js such that first test runs

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputStr) {
+    let outputStr = "";
+    for (i = -1; i >= (inputStr.length * -1); i--) {
+        outputStr += inputStr.at(i);
+    }
+    return outputStr;
 };
 
 // Do not edit below this line
