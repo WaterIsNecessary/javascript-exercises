e52d4575f0e4e6b2504f3c11af6f47a433982f7a
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-01-24 09:50:04 +0100

    UPDATE code such that first test passes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let inputStr = str;
+    for (i = 1; i < num; i++) {
+        str += inputStr;
+    }
+    return str;
 };
 
 // Do not edit below this line
