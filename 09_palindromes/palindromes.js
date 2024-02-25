cbc2cdc3f41934cb6e2bcc0b1e9525dd86eecf57
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-02-25 20:19:53 +0100

    ADD js solution for exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const rawArray = str.toLowerCase().split('');
+
+    const filteredArray = rawArray.filter((el,ind,arr) => {
+        if (el === " " || el === "." || el === "!" || el === ",") return false;
+        else return true;
+    });
+
+    const frontToBack = filteredArray.join('');
+    
+    const backToFront = filteredArray.reverse().join('');    
+
+    // remove these . " " ! ,
+    // lowercase
+    // now use str as a reference 
+
+    
+
+    return (backToFront === frontToBack) ? true : false;
+    
 };
 
 // Do not edit below this line
