2519f35ce13ad8ef416d987c95c1173a39ef2c4b
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-02-25 18:46:46 +0100

    ADD solutions to calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((accumulator,currentVal) => accumulator + currentVal,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator,currentVal) => accumulator * currentVal);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+  else {
+    let result = 1;
+    for (i = a; i >= 1; i--) {
+      result *= i
+    };
+    return result;
+  }
+   
 };
 
 // Do not edit below this line
