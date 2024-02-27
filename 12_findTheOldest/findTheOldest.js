481cfb61f5e389076b5994bcaaa7669eb0344d04
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-02-27 12:26:20 +0100

    ADD more efficient js solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
 const findTheOldest = function(array) {
 
-    const newArray = array.reduce((accumulator, currentVal, ind, arr) => {
+    // My initial way of doing it
+
+    /* const newArray = array.reduce((accumulator, currentVal, ind, arr) => {
 
         if (!currentVal.yearOfDeath) {
 
@@ -28,7 +30,30 @@ const findTheOldest = function(array) {
 
     });
 
-    return newArray[0];
+    return newArray[0]; */
+
+    // The other way of doing it
+
+    // write a function that calculates age and takes account of if the person
+    // is still alive
+
+    return array.reduce((oldest, currentPerson) => {
+        
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+        return (oldestAge > currentAge) ? oldest : currentPerson; 
+
+    });
+
+};
+
+const getAge = (birthYear, deathYear) => {
+
+    if (!deathYear) return new Date().getFullYear() - birthYear;
+
+    return deathYear - birthYear;        
 
 };
 
