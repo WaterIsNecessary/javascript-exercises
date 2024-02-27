991fe6a3b760bbfbad5eeafdffdf886e6f708d72
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-02-27 08:47:21 +0100

    ADD js solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+
+    const newArray = array.reduce((accumulator, currentVal, ind, arr) => {
+
+        if (!currentVal.yearOfDeath) {
+
+            // Amend this later to use JS date function
+
+            currentVal['age'] = (new Date().getFullYear()) - currentVal.yearOfBirth;
+
+        }
+
+        if (currentVal.yearOfDeath) {
+
+            currentVal['age'] = currentVal.yearOfDeath - currentVal.yearOfBirth;
+
+        }
+
+        accumulator[ind] = currentVal;
+
+        return accumulator;
+
+    },[]);
+
+    newArray.sort((a,b) => {
+
+        return (a.age > b.age) ? -1 : (a.age === b.age) ? 0 : 1;
+
+    });
+
+    return newArray[0];
 
 };
 
