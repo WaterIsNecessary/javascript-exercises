fd2bc4c1e08c034383c2ca67e5ef5bcb10481132
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-02-27 05:17:04 +0100

    UPDATE arg name for function exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,8 +1,8 @@
-const getTheTitles = function(books) {
+const getTheTitles = function(array) {
 
     // Good thinking but you can do this in a simpler way -- look below
 
-    /* let newArr = books.reduce((accumulator, currentValue, ind) => {
+    /* let newArr = array.reduce((accumulator, currentValue, ind) => {
         accumulator[ind] = currentValue.title;
         return accumulator;
     },[]);
@@ -12,7 +12,7 @@ const getTheTitles = function(books) {
     // The most simple way is to use the map function which has it inbuilt to
     // map the return values into an array of themselves
 
-    let newArr = books.map(ele => ele.title);
+    let newArr = array.map(ele => ele.title);
 
     return newArr;
 
