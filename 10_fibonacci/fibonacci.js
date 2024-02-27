b2225bb0c799583ee75f328f24b4844a9df76de7
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-02-27 04:13:45 +0100

    ADD js solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(rawMember) {
 
+    const filteredMember = +rawMember;
+
+    if (filteredMember === 0) {
+        return 0;
+    } else if (filteredMember < 0) {
+        return "OOPS";
+    } else {        
+
+        const fibonacciStart = [1,1];    
+
+        for (i = 0, j = 1; i < filteredMember; i++, j++) {
+
+            fibonacciStart.push(fibonacciStart[i] + fibonacciStart[j]);
+
+        }; 
+
+        return fibonacciStart[filteredMember - 1];
+    }
 };
 
 // Do not edit below this line
