0302978cee4fa7de06a70284b271faca8380494d
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-02-27 05:14:28 +0100

    ADD js solution to exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,20 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+
+    // Good thinking but you can do this in a simpler way -- look below
+
+    /* let newArr = books.reduce((accumulator, currentValue, ind) => {
+        accumulator[ind] = currentValue.title;
+        return accumulator;
+    },[]);
+
+    return newArr; */
+
+    // The most simple way is to use the map function which has it inbuilt to
+    // map the return values into an array of themselves
+
+    let newArr = books.map(ele => ele.title);
+
+    return newArr;
 
 };
 
